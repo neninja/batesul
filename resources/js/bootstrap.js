d0e1b7b67c00471417d6146f2901d7ba3559a0ab
Neni <felipe.dacs@gmail.com>
2024-11-17 02:17:56 -0300

    closes #4 #5

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,9 +8,9 @@ Alpine.start()
 
 Alpine.data('app', function() {
   return {
-    inicioExpediente: '',
-    inicioIntervalo: '',
-    finalIntervalo: '',
+    startWork: '',
+    startBreak: '',
+    endBreak: '',
     init() {
       this.loadTimes();
     },
@@ -22,46 +22,59 @@ Alpine.data('app', function() {
       this.saveTimes();
     },
     saveTimes() {
-      localStorage.setItem('inicioExpediente', this.inicioExpediente);
-      localStorage.setItem('inicioIntervalo', this.inicioIntervalo);
-      localStorage.setItem('finalIntervalo', this.finalIntervalo);
+      localStorage.setItem('startWork', this.startWork);
+      localStorage.setItem('startBreak', this.startBreak);
+      localStorage.setItem('endBreak', this.endBreak);
     },
     loadTimes() {
-      this.inicioExpediente = localStorage.getItem('inicioExpediente') || '';
-      this.inicioIntervalo = localStorage.getItem('inicioIntervalo') || '';
-      this.finalIntervalo = localStorage.getItem('finalIntervalo') || '';
+      this.startWork = localStorage.getItem('startWork') || '';
+      this.startBreak = localStorage.getItem('startBreak') || '';
+      this.endBreak = localStorage.getItem('endBreak') || '';
     },
     resetTimes() {
-      this.inicioExpediente = '';
-      this.inicioIntervalo = '';
-      this.finalIntervalo = '';
-      localStorage.removeItem('inicioExpediente');
-      localStorage.removeItem('inicioIntervalo');
-      localStorage.removeItem('finalIntervalo');
+      this.startWork = '';
+      this.startBreak = '';
+      this.endBreak = '';
+      localStorage.removeItem('startWork');
+      localStorage.removeItem('startBreak');
+      localStorage.removeItem('endBreak');
     },
-    get horarioParaSair() {
-      if (!this.inicioExpediente || !this.inicioIntervalo || !this.finalIntervalo) {
-        return 'Preencha todos os campos';
+    formatWithPad(number) {
+      return String(number).padStart(2, '0')
+    },
+    get endTime() {
+      console.clear()
+      if (!this.startWork || !this.startBreak || !this.endBreak) {
+        return 'Please fill in all fields';
       }
 
-      const [inicioExpedienteHours, inicioExpedienteMinutes] = this.inicioExpediente.split(':').map(Number);
-      const [inicioIntervaloHours, inicioIntervaloMinutes] = this.inicioIntervalo.split(':').map(Number);
-      const [finalIntervaloHours, finalIntervaloMinutes] = this.finalIntervalo.split(':').map(Number);
+      const [startWorkHours, startWorkMinutes] = this.startWork.split(':').map(Number);
+      const [startBreakHours, startBreakMinutes] = this.startBreak.split(':').map(Number);
+      const [endBreakHours, endBreakMinutes] = this.endBreak.split(':').map(Number);
 
-      const inicioExpedienteDate = new Date(0, 0, 0, inicioExpedienteHours, inicioExpedienteMinutes);
-      const inicioIntervaloDate = new Date(0, 0, 0, inicioIntervaloHours, inicioIntervaloMinutes);
-      const finalIntervaloDate = new Date(0, 0, 0, finalIntervaloHours, finalIntervaloMinutes);
+      const startBreakDate = new Date(0, 0, 0, startBreakHours, startBreakMinutes);
+      const endBreakDate = new Date(0, 0, 0, endBreakHours, endBreakMinutes);
 
-      const workDuration = 5 * 60 + 45; // 5 hours and 45 minutes in minutes
-      const intervaloDuration = (finalIntervaloDate - inicioIntervaloDate) / (1000 * 60); // Interval duration in minutes
+      const workDurationHours = 5;
+      const workDurationMinutes = 45;
+      console.log(`Duração de trabalho: ${this.formatWithPad(workDurationHours)}:${this.formatWithPad(workDurationMinutes)}`);
 
-      const totalMinutes = workDuration + intervaloDuration;
-      const endTime = new Date(inicioExpedienteDate.getTime() + totalMinutes * 60 * 1000);
+      const breakDurationMinutes = (endBreakDate - startBreakDate) / (1000 * 60);
+      const breakDurationHours = Math.floor(breakDurationMinutes / 60);
+      const breakDurationRemainingMinutes = breakDurationMinutes % 60;
+      console.log(`Duração do intervalo: ${this.formatWithPad(breakDurationHours)}:${this.formatWithPad(breakDurationRemainingMinutes)}`);
 
-      const hours = String(endTime.getHours()).padStart(2, '0');
-      const minutes = String(endTime.getMinutes()).padStart(2, '0');
+      const totalWorkMinutes = workDurationMinutes + breakDurationRemainingMinutes;
+      const totalWorkHours = workDurationHours + breakDurationHours + Math.floor(totalWorkMinutes / 60);
+      const totalWorkRemainingMinutes = totalWorkMinutes % 60;
+      console.log(`Total de trabalho necessário: ${this.formatWithPad(totalWorkHours)}:${this.formatWithPad(totalWorkRemainingMinutes)}`);
 
-      return `${hours}:${minutes}`;
+      const endTimeHours = startWorkHours + totalWorkHours + Math.floor((startWorkMinutes + totalWorkRemainingMinutes) / 60);
+      const endTimeMinutes = (startWorkMinutes + totalWorkRemainingMinutes) % 60;
+      console.log(`Horário iniciado: ${this.formatWithPad(startWorkHours)}:${this.formatWithPad(startWorkMinutes)}`);
+      console.log(`Horário finalização: ${this.formatWithPad(endTimeHours)}:${this.formatWithPad(endTimeMinutes)}`);
+
+      return `${this.formatWithPad(endTimeHours)}:${this.formatWithPad(endTimeMinutes)}`;
     }
   }
 })
